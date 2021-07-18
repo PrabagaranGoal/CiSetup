2ef3a02943da0980d50a8730ae8c2f445d336b0f
Prabagaran <nikprabha90@gmail.com>
2021-07-18 21:54:00 +0530

    first commit

---
+++ CiSetup/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CiSetup
+//
+//  Created by Prabhakar G on 18/07/21.
+//  Copyright © 2021 Prabhakar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
